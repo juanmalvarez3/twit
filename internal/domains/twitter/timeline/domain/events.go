ca8caa350fadf7c6708e224efdc52b83b760e101
Draftea juanmanuelalvarez <juanmalp3@gmail.com>
2025-06-11 20:55:18 -0300

    Update

--- internal/domains/twitter/timeline/domain/events.go
+++ internal/domains/twitter/timeline/domain/events.go
@@ -1,6 +1,6 @@
 package domain
 
 type PopulateCacheEvent struct {
-	UserID string `json:"userId"`
+	UserID string `json:"user_id"` // Cambiado de "userId" a "user_id" para coincidir con el payload
 	Source string `json:"source,omitempty"`
 }
